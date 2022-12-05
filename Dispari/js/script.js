272aaca3fffdf9e679d154a3714e6ee803637da6
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-05 19:14:33 +0100

    Ultimo push

--- Dispari/js/script.js
+++ Dispari/js/script.js
@@ -1,35 +1,46 @@
-let user_choose= prompt('Scegli se vuoi giocare con pari o dispari')
-let number_user= parseInt(prompt('Inserire un numero da 1 a 5'))
-console.log(number_user)
 
+let button = document.getElementById('save_data');
+//3 Condizioni per il calcolo del prezzo del biglietto in base ai km ed all'età
+button.addEventListener('click', function(){
 
-function generateNumber(min,max){
-   
-    let randomNumber= Math.floor(Math.random() * (max - min + 1) + min )
-    return randomNumber
+    let user_choose= document.getElementById('choose_id').value;
+    let number_user= document.getElementById('number_id').value;
     
-}
-let min = 1
-let max = 5
+    number_user = number_user * 1;
+   
+    console.log(number_user)
+    
+    
+    function generateNumber(min,max){
+       
+        let randomNumber= Math.floor(Math.random() * (max - min + 1) + min )
+        return randomNumber
+        
+    }
+    let min = 1
+    let max = 5
+    
+    let number = generateNumber(min,max);
+    console.log(number)
+    
+    let sum = number_user + number
+    console.log(sum)
+    
+    
+    if(sum % 2 === 0 && user_choose === 'pari'){
+        alert('Hai vinto')
+    }
+    
+    else if(sum % 2 !== 0 && user_choose === 'dispari'){
+        alert('Hai vinto')
+    }
+    
+    else {
+        alert('Hai perso')
+    }
 
-let number= generateNumber(min,max);
-console.log(number)
 
-let sum = number_user + number
-console.log(sum)
 
-if(number_user == number){
-    alert('Pareggio')
-}
 
-else if(sum % 2 === 0 && user_choose === 'pari'){
-    alert('Hai vinto')
-}
+})
 
-else if(sum % 2 !== 0 && user_choose === 'dispari'){
-    alert('Hai vinto')
-}
-
-else {
-    alert('Hai perso')
-}
