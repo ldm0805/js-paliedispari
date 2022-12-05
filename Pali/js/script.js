b795a26c38062d31d70ac79fbf7138bd26916b52
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-05 17:50:58 +0100

    Esercizio pali completato

---
+++ Pali/js/script.js
@@ -0,0 +1,20 @@
+
+//Creare la funzione per effettuare il controllo della palindroma
+function Palindrome(word) {
+    let word_invert = word.split('').reverse().join('');
+    return word_invert
+}
+
+//Chiedere all'utente una parola e renderla in caratteri minuscoli
+const word_input = prompt("Inserisci una parola").toLowerCase();
+
+
+let check_word = Palindrome(word_input)
+
+// Ciclo che controlla che la parola sia palindroma
+if(word_input == check_word){
+    console.log("si")
+}
+else{
+    console.log("no")
+}
---
+++ Pali/readme.txt
@@ -0,0 +1,7 @@
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+1. Chiedere all'utente una parola
+2. Rendere quella parola tutta minuscola
+3. Scomporla per il controllo
+4. Effettuare il controllo
+5. Stampare la risposta 
